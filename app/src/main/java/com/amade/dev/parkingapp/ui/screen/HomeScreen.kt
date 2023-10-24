f9b626a20aa0263d26f4eaff920d3b6e2de82a2a
AmadeAli1 <aliamade29@gmail.com>
2023-10-24 09:26:28 +0200

    Initial commit

--- app/src/main/java/com/amade/dev/parkingapp/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/amade/dev/parkingapp/ui/screen/HomeScreen.kt
@@ -1,12 +1,14 @@
 package com.amade.dev.parkingapp.ui.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.outlined.LocalParking
@@ -15,14 +17,17 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -59,29 +64,48 @@ fun HomeScreen(navController: NavController) {
                         Icon(imageVector = Icons.Default.ExitToApp, contentDescription = null)
                     }
                 })
+        }, floatingActionButton = {
+            LargeFloatingActionButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.size(80.dp),
+                containerColor = MaterialTheme.colorScheme.tertiaryContainer
+            ) {
+                Icon(imageVector = Icons.Outlined.LocalParking, contentDescription = null)
+            }
         }
     ) { padding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)
+                .padding(14.dp), verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
 
             Card(onClick = { /*TODO*/ }) {
-                Box{
+                Box {
                     Image(
                         painter = painterResource(id = R.drawable.parking001),
                         contentDescription = null,
                         modifier = Modifier
                             .fillMaxWidth()
-                            .height(250.dp),
+                            .height(230.dp),
                         contentScale = ContentScale.Crop
                     )
-                    
+                    Text(
+                        text = "Parking History",
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(8.dp),
+                        style = MaterialTheme.typography.headlineLarge.copy(
+                            fontWeight = FontWeight.ExtraBold,
+                            color = MaterialTheme.colorScheme.onSecondary
+                        )
+                    )
                 }
             }
 
         }
     }
+
 
 }
